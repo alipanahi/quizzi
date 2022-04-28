25ce39374fbfddfa07dc39fc66a36ae50a1c2430
ali h panahi <p.alihussain@gmail.com>
2022-04-28 17:12:58 +0200

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,16 +1,13 @@
 import React from "react"
-export default function Form(props){
+export default class Form extends React.Component{
+    state = {
+        category:"",
+        difficulty:""
+    }
     
-    const [field,setField] = React.useState(
-        {
-            category:"",
-            difficulty:""
-        }
-    )
-    
-    function handleChange(event){
+    handleChange = event=>{
         const {name, value} = event.target
-        setField(prevDate=>{
+        this.setState(prevDate=>{
             return {
                 ...prevDate,
                 [name]: value
@@ -18,34 +15,36 @@ export default function Form(props){
         })
     }
 
-    function handleSubmit(event){
+    handleSubmit = event =>{
         event.preventDefault()
-        props.handleData(field)
+        this.props.handleData(this.state)
     }
-    return(
-        <form onSubmit={handleSubmit}>
-            <h1>Quizzical</h1>
-            <select className="select" name="category" onChange={handleChange} value={field.category}>
-                <option value="">Select Category</option>
-                <option value="18">Science: computers</option>
-                <option value="21">Sports</option>
-                <option value="9">General Knowledge</option>
-                <option value="11">Entertainment: Film</option>
-                <option value="12">Entertainment: Music</option>
-                <option value="32">Entertainment: Cartoon & Animations</option>
-                <option value="22">Geography</option>
-                <option value="26">Celebrities</option>
-                
-            </select>
-            <br />
-            <select className="select" name="difficulty" onChange={handleChange} value={field.difficulty}>
-                <option value="">Select Difficulty</option>
-                <option value="easy">Easy</option>
-                <option value="medium">Medium</option>
-                <option value="hard">Hard</option>
-            </select>
-            <br />
-            <button  className="btn">start Quiz</button>
-        </form>
-    )
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <h1>Quizzical</h1>
+                <select className="select" name="category" onChange={this.handleChange} value={this.state.category}>
+                    <option value="">Select Category</option>
+                    <option value="18">Science: computers</option>
+                    <option value="21">Sports</option>
+                    <option value="9">General Knowledge</option>
+                    <option value="11">Entertainment: Film</option>
+                    <option value="12">Entertainment: Music</option>
+                    <option value="32">Entertainment: Cartoon & Animations</option>
+                    <option value="22">Geography</option>
+                    <option value="26">Celebrities</option>
+                    
+                </select>
+                <br />
+                <select className="select" name="difficulty" onChange={this.handleChange} value={this.state.difficulty}>
+                    <option value="">Select Difficulty</option>
+                    <option value="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="hard">Hard</option>
+                </select>
+                <br />
+                <button  className="btn">start Quiz</button>
+            </form>
+        )
+    }
 }
